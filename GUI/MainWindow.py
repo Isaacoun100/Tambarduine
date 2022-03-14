b189c4e9307ed2826987b5e5680ca8f36ef67cc2
Isaacoun100 <Isaacoun100@estudiantec.cr>
2022-03-13 22:03:30 -0600

    Main IDLE Window created

---
+++ GUI/MainWindow.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+#Main IDE Window
+main_window = Tk()
+
+#The main window will run at 1080p resolution and will not be resizable
+main_window.geometry("1920x1080")
+main_window.resizable(False, False)
+
+#Window declaration
+main_window.mainloop()
