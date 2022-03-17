0a35f104d1631fb24951b30468e1f546f0ca9657
Isaacoun100 <Isaacoun100@estudiantec.cr>
2022-03-16 21:41:32 -0600

    Console menu added
    The user can now visualize the errors given by the compiler

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import simpledialog
 
 class IDE:
     def __init__(self):
@@ -18,24 +19,9 @@ class IDE:
         #If true fullscreen is disable this line will show the window title
         self.ide_window.title("Tambarduine IDE")
 
-        #Escape sequence with the Esc button
-        self.ide_window.bind('<Escape>', lambda e: self.onExit() )
-
         #Entry where the code will be
         self.code_Text=Text()
-
-    def startIDE(self):
-
-        #Start the Top Bar
-        self.topBar()
-
-        #Start the program canvas
-        self.codeCanvas()
-
-        #Window declaration.
-        self.ide_window.mainloop()
-
-    def codeCanvas(self):
+        self.output_Text=Text()
 
         #Canvas creation
         myCanvas = Canvas(self.ide_window,
@@ -45,13 +31,33 @@ class IDE:
         self.darkMode(myCanvas)
         myCanvas.pack(fill="both", expand=True)
 
+        #Key commands
+        self.ide_window.bind('<Escape>', lambda e: self.onExit() )
+        self.ide_window.bind('<F5>', lambda e: self.getCode() )
+
+    def startIDE(self):
+
+        #Start the Top Bar
+        self.topBar()
+
+        #Start the program canvas
+        self.codeCanvas()
+
+        #Set the output text console
+        self.console()
+
+        #Window declaration.
+        self.ide_window.mainloop()
+
+    def codeCanvas(self):
+
         #Entry where the user can program in Tambarduine
         self.code_Text = Text(self.ide_window,
             font=("Helvetica", 14),
-            bg="#282C34", bd=0,
-            fg="#FFFFFF",highlightthickness=0)
+            bg="#282C34", bd=1, #282C34
+            fg="#FFFFFF",highlightthickness=1)
 
-        self.code_Text.place(x=10, y=10, width=1850, height=800)
+        self.code_Text.place(x=20, y=10, width=1850, height=800)
 
     def topBar(self):
         #This is the top bar where the program can be compiled, run, saved or opened
@@ -71,7 +77,7 @@ class IDE:
         filemenu.add_command(label="  Save  ", command = self.onSave)
         filemenu.add_command(label="  Exit  ", command = self.onExit)
 
-        #The file menu is being defined
+        #The run menu is being defined
         runmenu = Menu(menubar)
 
         #Runmenu created
@@ -80,6 +86,16 @@ class IDE:
         #Runmenu options definition
         runmenu.add_command(label="  Compile  ", command = self.getCode)
         runmenu.add_command(label="  Compile & Run  ", command = self.onExit)
+
+        #The Edit menu is being defined
+        editmenu = Menu(menubar)
+
+        #Runmenu created
+        menubar.add_cascade(label="  Options  ", menu=editmenu)
+
+        #Runmenu options definition
+        editmenu.add_command(label="  Go to line  ", command = self.askGoTo)
+
 
     def onOpen(self):
         print(filedialog.askopenfilename(initialdir = "/",title = "Open file",filetypes = (("Python files","*.py;*.pyw"),("All files","*.*"))))
@@ -100,4 +116,32 @@ class IDE:
 
     def getCode(self):
         code = self.code_Text.get("1.0",END)
-        print(code)
+        self.setConsole(code)
+
+    def console(self):
+
+        #Entry where the user can program in Tambarduine
+        self.output_Text = Text(self.ide_window,
+            bg="#414752", bd=1,
+            fg="#FFFFFF",highlightthickness=1)
+        self.output_Text.place(x=20, y=850, width=1850, height=175)
+
+        self.setConsole("Welcome to the Tambarduine integrated development enviroment")
+
+    def setConsole(self, message):
+
+        #Write on the read only console
+        self.output_Text.config(state=NORMAL)
+        self.output_Text.insert(END, '\n'+message)
+        self.output_Text.config(state=DISABLED)
+
+    def goToLine(self, line, column):
+        self.code_Text.mark_set(INSERT, "%d.%d" % (line + 1, column + 1))
+        self.code_Text.focus_set()
+
+    def askGoTo(self):
+        line = simpledialog.askstring("Line", "Find line", parent=self.ide_window)
+        if line is not None:
+            column = simpledialog.askstring("Column", "Find column", parent=self.ide_window)
+            if column is not None:
+                self.goToLine(int(line), int(column))
